7be8a6542c5d488a9b4b906d2c2684b9fd3a3f1e
Josh236061 <71394743+Josh236061@users.noreply.github.com>
2022-10-10 14:13:13 -0500

    Updated 'generateMarkdown.js' File

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,79 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const fs = require('fs');
 
-// TODO: Create a function that returns the license link
+// Returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+    return ``;
+  } else {
+    return `[![${license} license](https://img.shields.io/badge/License-${license}-blue.svg)](${renderLicenseLink(license)})`
+  }
+}
 
-// TODO: Create a function that returns the license section of README
+// Returns the license link
+function renderLicenseLink(license) {
+  if (license === 'MIT') {
+    return `https://lbesson.mit-license.org/`
+  }
+  if (license === 'GPL') {
+    return `http://perso.crans.org/besson/LICENSE.html`
+  }
+  if (license === 'CC--0') {
+    return `https://creativecommons.org/licenses/by-nd/4.0` 
+  }
+}
+
+// Returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return ``;
+  } else {
+    return `## Licenses
+    This project is covered under the ${license} license. To learn more about what this means, click the license button at the top.`
+  }
+}
 
-// TODO: Create a function to generate markdown for README
+// Generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ${renderLicenseBadge(data.licenses)}
+
+  ## Table of Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Licenses](#licenses)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  * [Credits](#credits)
+
+  ## Description
+  ${data.description}
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ${renderLicenseSection(data.licenses)}
+
+  ## Contributing
+  ${data.contributing}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  Have questions about this project?  
+  GitHub: https://github.com/${data.github}  
+  Email: ${data.email}
+
+  ## Credits
+  ${data.name}
 `;
 }
 
